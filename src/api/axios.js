f4dae6bdee8c4e735620c7708a262db3755a7331
Harshit Tripathi <ht4065768@gmail.com>
2026-08-30 22:13:12 +0530

    chore: clean project structure

--- src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
-  withCredentials: true, // send the httpOnly cookie with requests
-});
-
-export default api;
